d915abd775f929cd8dfe11417e99cc2dafcd82c7
GLdongdong <2970665812@qq.com>
2019-12-29 13:35:38 +0800

    3

---
+++ src/test/java/com/swjtuhc/demo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swjtuhc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
